5682663cae902ebc9d5d2842a3337abcded963bb
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2025-09-04 17:10:24 -0400

    test

---
+++ lab_28_08/index.js
@@ -0,0 +1,14 @@
+const users = [
+    {id:1, name:"Ana",age:22},
+    {id:2, name:"Luis",age:19}
+];
+
+const byAge = users.reduce((acc,u) =>{
+    if(acc[u.age]<20){
+        acc[u.age]=[];        
+    }
+    acc[u.age].push(u.name);
+    return acc;
+},{});
+
+console.log(byAge)
